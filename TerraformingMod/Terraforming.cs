18d03b9eb038fd02a6462eef3dfaa518157ab49f
Elmotrix <kim.hoftvedt@gmail.com>
2023-01-19 01:14:52 +0100

    .

--- TerraformingMod/Terraforming.cs
+++ TerraformingMod/Terraforming.cs
@@ -10,34 +10,6 @@ using Assets.Scripts.Serialization;
 
 namespace TerraformingMod
 {
-    #region BepInEx
-    [BepInEx.BepInPlugin(pluginGuid, pluginName, pluginVersion)]
-    public class TerraformingMod : BepInEx.BaseUnityPlugin
-    {
-        public const string pluginGuid = "net.elmo.stationeers.Terraforming";
-        public const string pluginName = "Terraforming Mod";
-        public const string pluginVersion = "1.0";
-        public static void Log(string line)
-        {
-            Debug.Log("[" + pluginName + "]: " + line);
-        }
-        void Awake()
-        {
-            try
-            {
-                var harmony = new Harmony(pluginGuid);
-                harmony.PatchAll();
-                Log("Patch succeeded");
-            }
-            catch (Exception e)
-            {
-                Log("Patch Failed");
-                Log(e.ToString());
-            }
-        }
-    }
-    #endregion
-
     [HarmonyPatch(typeof(Atmosphere), "GiveAtmospherePortion")]
     public class AtmosphereGiveAtmospherePortionPatch
     {
